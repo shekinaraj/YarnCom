b669c54ae24216f161382ef9c37936c6a7ae9336
dawnsys <dawnsys@LAPTOP-G6GBQQSK>
2017-11-24 12:00:51 +0530

    updated

--- YarncomsProject/src/com/yarncoms/model/Customer.java
+++ YarncomsProject/src/com/yarncoms/model/Customer.java
@@ -43,8 +43,11 @@ public class Customer {
 	@Column(name = "contactPersonEmail")
 	private String contactPersonEmail;
 
+	@Column(name = "countryCode")
+	private String countryCode;
+	
 	@Column(name = "mobileNo")
-	private long mobileNo;
+	private String mobileNo;
 
 	@Column(name = "whatsApp")
 	private long whatsApp;
@@ -156,11 +159,19 @@ public class Customer {
 		this.contactPersonEmail = contactPersonEmail;
 	}
 
-	public long getMobileNo() {
+	public String getCountryCode() {
+		return countryCode;
+	}
+
+	public void setCountryCode(String countryCode) {
+		this.countryCode = countryCode;
+	}
+
+	public String getMobileNo() {
 		return mobileNo;
 	}
 
-	public void setMobileNo(long mobileNo) {
+	public void setMobileNo(String mobileNo) {
 		this.mobileNo = mobileNo;
 	}
 
