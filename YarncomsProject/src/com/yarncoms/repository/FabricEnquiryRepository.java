9cc20c7989add72fb971444050392bf1f13635a3
dawnsys <dawnsys@LAPTOP-G6GBQQSK>
2017-11-26 02:24:32 +0530

    updated on 26-11-2017

--- YarncomsProject/src/com/yarncoms/repository/FabricEnquiryRepository.java
+++ YarncomsProject/src/com/yarncoms/repository/FabricEnquiryRepository.java
@@ -16,5 +16,5 @@ public interface FabricEnquiryRepository extends JpaRepository<FabricEnquiry, Lo
 	
 	@Query("select fp from FabricProduct fp, FabricEnquiry fe where fe.enquiryId=?1 and fe.warpPly=fp.warpPly and fe.warpCount=fp.warpCount and fe.weftCount=fp.weftCount and fe.epi=fp.epi and fe.ppi=fp.ppi and fe.width=fp.width and fe.weaveType=fp.weaveType and fe.selvedge=fp.selvedge and fe.material=fp.material and fe.construction=fp.construction")
 	List<FabricEnquiry> getByQuery(Long fabricEnquiryId);
-
+ 
 }
